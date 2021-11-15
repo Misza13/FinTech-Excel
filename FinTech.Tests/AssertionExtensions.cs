5a81b7987f4e2a3c8d58af3c5dda6b6bec113ee5
Misza <misza@misza.net>
2021-11-15 16:15:09 +0100

    Calculation of all common greeks

---
+++ FinTech.Tests/AssertionExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace FinTech.Tests
+{
+    using System;
+    using FluentAssertions;
+    using FluentAssertions.Numeric;
+
+    public static class AssertionExtensions
+    {
+        public static AndConstraint<NumericAssertions<double>> BeApproximatelyS(
+            this NumericAssertions<double> parent,
+            double expectedValue,
+            int significantDigits)
+        {
+            var size = (int) Math.Floor(Math.Log10(expectedValue));
+            return parent.BeApproximately(expectedValue, Math.Pow(10, size - 4));
+        }
+    }
+}
--- FinTech/OptionsFunctions.cs
+++ FinTech/OptionsFunctions.cs
@@ -37,10 +37,9 @@
                 Description = "Risk-free interest rate")]
             double r)
         {
-            var dPlus = (Math.Log(S / K) + (r + sigma * sigma / 2) * T) / (sigma * Math.Sqrt(T));
-            var dMinus = dPlus - sigma * Math.Sqrt(T);
-            return S * Phi(dPlus) -
-                   K * Phi(dMinus);
+            var (dPlus, dMinus) = DPlusMinus(S, K, T, sigma, r);
+            return + S * Phi(dPlus)
+                   - K * Math.Exp(-r * T) * Phi(dMinus);
         }
         
         [ExcelFunction(
@@ -72,15 +71,220 @@
                 Description = "Risk-free interest rate")]
             double r)
         {
-            var dPlus = (Math.Log(S / K) + (r + sigma * sigma / 2) * T) / (sigma * Math.Sqrt(T));
-            var dMinus = dPlus - sigma * Math.Sqrt(T);
-            return -S * Phi(-dPlus) +
-                   K * Math.Exp(-r * T) * Phi(-dMinus);
+            var (dPlus, dMinus) = DPlusMinus(S, K, T, sigma, r);
+            return - S * Phi(-dPlus)
+                   + K * Math.Exp(-r * T) * Phi(-dMinus);
         }
 
-        private static double Phi(double x)
+        [ExcelFunction(
+            Name = "Options.DeltaEuroCall",
+            Description = "Delta of an european CALL option using B-S formula")]
+        public static double DeltaEuroCall(
+            [ExcelArgument(
+                Name = "S",
+                Description = "Price of underlying instrument")]
+            double S,
+
+            [ExcelArgument(
+                Name = "K",
+                Description = "Option strike price")]
+            double K,
+
+            [ExcelArgument(
+                Name = "T",
+                Description = "Time to option expiration (in years)")]
+            double T,
+
+            [ExcelArgument(
+                Name = "sigma",
+                Description = "Annual volatility of underlying instrument")]
+            double sigma,
+
+            [ExcelArgument(
+                Name = "r",
+                Description = "Risk-free interest rate")]
+            double r)
         {
-            return Normal01.CumulativeDistribution(x);
+            var (dPlus, _) = DPlusMinus(S, K, T, sigma, r);
+            return Phi(dPlus);
         }
+        
+        [ExcelFunction(
+            Name = "Options.DeltaEuroPut",
+            Description = "Delta of an european PUT option using B-S formula")]
+        public static double DeltaEuroPut(
+            [ExcelArgument(
+                Name = "S",
+                Description = "Price of underlying instrument")]
+            double S,
+
+            [ExcelArgument(
+                Name = "K",
+                Description = "Option strike price")]
+            double K,
+
+            [ExcelArgument(
+                Name = "T",
+                Description = "Time to option expiration (in years)")]
+            double T,
+
+            [ExcelArgument(
+                Name = "sigma",
+                Description = "Annual volatility of underlying instrument")]
+            double sigma,
+
+            [ExcelArgument(
+                Name = "r",
+                Description = "Risk-free interest rate")]
+            double r)
+        {
+            var (dPlus, _) = DPlusMinus(S, K, T, sigma, r);
+            return -Phi(-dPlus);
+        }
+        
+        [ExcelFunction(
+            Name = "Options.GammaEuro",
+            Description = "Gamma of an european option using B-S formula")]
+        public static double GammaEuro(
+            [ExcelArgument(
+                Name = "S",
+                Description = "Price of underlying instrument")]
+            double S,
+
+            [ExcelArgument(
+                Name = "K",
+                Description = "Option strike price")]
+            double K,
+
+            [ExcelArgument(
+                Name = "T",
+                Description = "Time to option expiration (in years)")]
+            double T,
+
+            [ExcelArgument(
+                Name = "sigma",
+                Description = "Annual volatility of underlying instrument")]
+            double sigma,
+
+            [ExcelArgument(
+                Name = "r",
+                Description = "Risk-free interest rate")]
+            double r)
+        {
+            var (dPlus, _) = DPlusMinus(S, K, T, sigma, r);
+            return Norm(dPlus) / (S * sigma * Math.Sqrt(T));
+        }
+        
+        [ExcelFunction(
+            Name = "Options.ThetaEuroCall",
+            Description = "Theta of an european CALL option using B-S formula")]
+        public static double ThetaEuroCall(
+            [ExcelArgument(
+                Name = "S",
+                Description = "Price of underlying instrument")]
+            double S,
+
+            [ExcelArgument(
+                Name = "K",
+                Description = "Option strike price")]
+            double K,
+
+            [ExcelArgument(
+                Name = "T",
+                Description = "Time to option expiration (in years)")]
+            double T,
+
+            [ExcelArgument(
+                Name = "sigma",
+                Description = "Annual volatility of underlying instrument")]
+            double sigma,
+
+            [ExcelArgument(
+                Name = "r",
+                Description = "Risk-free interest rate")]
+            double r)
+        {
+            var (dPlus, dMinus) = DPlusMinus(S, K, T, sigma, r);
+            return (-S * Norm(dPlus) * sigma / (2 * Math.Sqrt(T))
+                    - r * K * Math.Exp(-r * T) * Phi(dMinus)) / 365;
+        }
+        
+        [ExcelFunction(
+            Name = "Options.ThetaEuroPut",
+            Description = "Theta of an european PUT option using B-S formula")]
+        public static double ThetaEuroPut(
+            [ExcelArgument(
+                Name = "S",
+                Description = "Price of underlying instrument")]
+            double S,
+            
+            [ExcelArgument(
+                Name = "K",
+                Description = "Option strike price")]
+            double K,
+            
+            [ExcelArgument(
+                Name = "T",
+                Description = "Time to option expiration (in years)")]
+            double T,
+            
+            [ExcelArgument(
+                Name = "sigma",
+                Description = "Annual volatility of underlying instrument")]
+            double sigma,
+            
+            [ExcelArgument(
+                Name = "r",
+                Description = "Risk-free interest rate")]
+            double r)
+        {
+            var (dPlus, dMinus) = DPlusMinus(S, K, T, sigma, r);
+            return (- S * Norm(dPlus) * sigma / 2 / Math.Sqrt(T)
+                    + r * K * Math.Exp(-r * T) * Phi(-dMinus)) / 365;
+        }
+        
+        [ExcelFunction(
+            Name = "Options.VegaEuro",
+            Description = "Vega of an european option using B-S formula")]
+        public static double VegaEuro(
+            [ExcelArgument(
+                Name = "S",
+                Description = "Price of underlying instrument")]
+            double S,
+
+            [ExcelArgument(
+                Name = "K",
+                Description = "Option strike price")]
+            double K,
+
+            [ExcelArgument(
+                Name = "T",
+                Description = "Time to option expiration (in years)")]
+            double T,
+
+            [ExcelArgument(
+                Name = "sigma",
+                Description = "Annual volatility of underlying instrument")]
+            double sigma,
+
+            [ExcelArgument(
+                Name = "r",
+                Description = "Risk-free interest rate")]
+            double r)
+        {
+            var (dPlus, _) = DPlusMinus(S, K, T, sigma, r);
+            return S * Norm(dPlus) * Math.Sqrt(T) / 100;
+        }
+
+        private static (double, double) DPlusMinus(double S, double K, double T, double sigma, double r)
+        {
+            var dPlus = (Math.Log(S / K) + (r + sigma * sigma / 2) * T) / (sigma * Math.Sqrt(T));
+            var dMinus = dPlus - sigma * Math.Sqrt(T);
+            return (dPlus, dMinus);
+        }
+
+        private static double Norm(double x) => Normal01.Density(x);
+
+        private static double Phi(double x) => Normal01.CumulativeDistribution(x);
     }
 }
